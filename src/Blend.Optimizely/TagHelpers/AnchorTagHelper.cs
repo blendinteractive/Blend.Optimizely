e39df185f6bc3bd2d7448931f21a8632e82f151a
John Pavek <John.Pavek@blendinteractive.com>
2024-06-20 10:45:52 -0500

    Anchor helper update

--- src/Blend.Optimizely/TagHelpers/AnchorTagHelper.cs
+++ src/Blend.Optimizely/TagHelpers/AnchorTagHelper.cs
@@ -1,4 +1,5 @@
-﻿using EPiServer.ServiceLocation;
+using Blend.Optimizely;
+using EPiServer.ServiceLocation;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace Blend.Optimizely.TagHelpers
@@ -19,51 +20,70 @@ namespace Blend.Optimizely.TagHelpers
         [HtmlAttributeName("content-link")]
         public object ContentLink { get; set; }
 
+        /// <summary>
+        /// How should the anchor tag be handle when there is no valid href or the condition is false. 
+        /// Options are:
+        /// None, ConvertLinkToSpan, SuppressOutput, KeepInnerContent
+        /// </summary>
         [HtmlAttributeName("link-options")]
         public LinkOptions LinkOptions { get; set; }
 
-        [HtmlAttributeName("output-options")]
-        public OutputOptions OutputOptions { get; set; }
+        [HtmlAttributeName("fallback-option")]
+        public AnchorFallbackOptions FallbackOption { get; set; }
+
+        /// <summary>
+        /// Only output an anchor tag with href when condition is true and a valid link exists. Default is true
+        /// </summary>
+        [HtmlAttributeName("condition")]
+        public bool Condition { get; set; } = true;
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            var resolvedLink = LinkResolver.Service.TryResolveLink(ContentLink, LinkOptions);
-
-            if (resolvedLink is null)
+            if (context is null || output is null)
                 return;
 
-            if (resolvedLink.Href.HasValue())
+            var resolvedLink = LinkResolver.Service.TryResolveLink(ContentLink, LinkOptions);
+            if (resolvedLink is not null && resolvedLink.Href.HasValue() && Condition)
             {
                 output.Attributes.SetAttribute("href", resolvedLink.Href);
+
+                if (resolvedLink.Target.HasValue() && !context.AllAttributes.TryGetAttribute("target", out _))
+                    output.Attributes.SetAttribute("target", resolvedLink.Target);
             }
-            else if (OutputOptions is OutputOptions.ConvertLinkToSpan)
+            else if (FallbackOption is AnchorFallbackOptions.ConvertLinkToSpan)
             {
                 output.TagName = "span";
             }
-            else if (OutputOptions is OutputOptions.SuppressOutput)
+            else if (FallbackOption is AnchorFallbackOptions.SuppressOutput)
             {
                 output.SuppressOutput();
                 return;
             }
-
-            if (resolvedLink.Target.HasValue() && !context.AllAttributes.TryGetAttribute("target", out _))
-                output.Attributes.SetAttribute("target", resolvedLink.Target);
+            else if (FallbackOption is AnchorFallbackOptions.KeepInnerContent)
+            {
+                output.TagName = string.Empty;
+                return;
+            }
         }
     }
 
-
-    public enum OutputOptions
+    public enum AnchorFallbackOptions
     {
         None = 0,
 
         /// <summary>
-        /// If url is blank, convert the wrapping <a> to a <span>
+        /// If url is blank, convert the wrapping a tag to a span
         /// </summary>
         ConvertLinkToSpan = 1,
 
         /// <summary>
         /// If url is blank, suppress output
         /// </summary>
-        SuppressOutput = 2
+        SuppressOutput = 2,
+
+        /// <summary>
+        /// If url is blank, remove wrapping tag
+        /// </summary>
+        KeepInnerContent = 3
     }
 }
